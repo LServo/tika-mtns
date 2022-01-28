a62e5a0db38f2f469e76e430b36db98bb43f6d35
Lucas Servo <l.servo@hotmail.com>
2022-01-28 20:06:12 -0300

    fix => fixed user.ts

---
+++ src/modules/accounts/entity/user.ts
@@ -0,0 +1,27 @@
+
+class User {
+    constructor
+    
+    /**
+     * id
+     * nick
+     * email
+     * conta
+     * isAdmin
+     * coplayers Coplayer[]
+     * created_at
+     */
+}
+
+class Account {
+    nickname: Tikabum
+    user_admin_id: [new User(Lucas).ID]
+    password: string,
+    hash_password: string,
+    coplayers: [new User(Gabriel), new User(Lucas)],
+}
+
+// Tikabum = Gabriel e o Lucas
+
+// Tika - User => O que um user pode fazer diferente do coplayer?
+// Mtns - User => 
